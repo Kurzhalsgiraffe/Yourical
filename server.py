ed38dc50fa9084c17136f211b30a315a5394e8ef
Kurzhalsgiraffe <max-hantschke@web.de>
2024-03-20 23:55:15 +0100

    :)

--- server.py
+++ server.py
@@ -94,4 +94,4 @@ def register():
 ## ----- MAIN ----- ##
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000, ssl_context=('ssl/certificate.crt', 'ssl/privatekey.key'))
+    app.run(debug=True, host="0.0.0.0", port=443, ssl_context=('ssl/certificate.crt', 'ssl/privatekey.key'))
