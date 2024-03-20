2cf2103f7d1dbc0f25fdc90e8467b63914a1bd92
Kurzhalsgiraffe <max-hantschke@web.de>
2024-03-20 23:50:01 +0100

    Create ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,3 @@
+openssl genpkey -algorithm RSA -out privatekey.key
+openssl req -new -key privatekey.key -out csr.pem
+openssl req -x509 -sha256 -days 365 -key privatekey.key -in csr.pem -out certificate.crt
