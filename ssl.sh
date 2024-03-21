2caaf3e0f3ec5450b4505b0d87698e226020ad49
Kurzhalsgiraffe <max-hantschke@web.de>
2024-03-21 22:36:29 +0100

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+mkdir ssl
+cd ssl
 openssl genpkey -algorithm RSA -out privatekey.key
 openssl req -new -key privatekey.key -out csr.pem
 openssl req -x509 -sha256 -days 365 -key privatekey.key -in csr.pem -out certificate.crt
