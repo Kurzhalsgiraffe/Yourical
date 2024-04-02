e7189a2795100a201613143a38fd801ba2b91240
Kurzhalsgiraffe <max-hantschke@web.de>
2024-04-02 16:49:46 +0200

    minor change

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -92,7 +92,7 @@ function submitDateForm() {
 
 function copyLink(event) {
     event.preventDefault();
-    
+
     var link = event.target.href;
     navigator.clipboard.writeText(link);
     alert("Link copied to clipboard: " + link);
