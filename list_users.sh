987a0cd66412a090cd1a33580f7796d96e81b973
Kurzhalsgiraffe <max-hantschke@web.de>
2024-04-18 13:27:57 +0200

    Update list_users.sh

--- list_users.sh
+++ list_users.sh
@@ -6,5 +6,11 @@ DB_FILE="instance/database.db"
 # Define your SQL query
 SQL_QUERY="SELECT username FROM user;"
 
-# Execute the query using sqlite3 command and print the result
-sqlite3 "$DB_FILE" "$SQL_QUERY"
+# Execute the query using sqlite3 command with desired output formatting options
+sqlite3 "$DB_FILE" <<EOF
+.mode column
+.headers on
+.separator ROW "\n"
+.nullvalue NULL
+$SQL_QUERY
+EOF
