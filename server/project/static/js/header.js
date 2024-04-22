a415af9d3e006aa82623df458631f6425fb93c54
Kurzhalsgiraffe <max-hantschke@web.de>
2024-04-22 17:58:32 +0200

    aufräumen

--- server/project/static/js/header.js
+++ server/project/static/js/header.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $(document).ready(function () {
         var burger = $('#id_burger');
-    
+
         burger.click(function() {
             var navbar = $('#id_navbar1');
             navbar.toggle(); // Toggle between display block and none
