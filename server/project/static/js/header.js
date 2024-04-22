a07450297b4aefd91fbc28d2978964cdf033d81d
Wunkelbold <silas-feu@outlook.com>
2024-04-22 03:57:55 +0200

    css navbar anpassung

---
+++ server/project/static/js/header.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(document).ready(function () {
+        var burger = $('#id_burger');
+    
+        burger.click(function() {
+            var navbar = $('#id_navbar1');
+            navbar.toggle(); // Toggle between display block and none
+        });
+    });
+});
