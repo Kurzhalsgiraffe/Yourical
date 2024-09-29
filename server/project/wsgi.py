f1c8c5f113c264848bb97818ebaf534566eb2a26
Kurzhalsgiraffe <max-hantschke@web.de>
2024-09-30 00:23:14 +0200

    Update wsgi.py

--- server/project/wsgi.py
+++ server/project/wsgi.py
@@ -1,4 +1,4 @@
 from server import app
 from server import update_calendars
 
-# update_calendars() # This will start the update when the Server is started. Disabled while Untis is buggy, because server will not start when this crashes!
+update_calendars() # This will start the update when the Server is started. Disable it while API is bugged, because when this fails the server wont even start
