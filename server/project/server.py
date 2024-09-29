7a0858edb620576f85be6620a36dc81a1ae33489
Kurzhalsgiraffe <max-hantschke@web.de>
2024-09-29 22:53:20 +0200

    wsgi compacted

--- server/project/server.py
+++ server/project/server.py
@@ -245,5 +245,4 @@ def update_calendars():
 ## ----- MAIN ----- ##
 
 if __name__ == "__main__":
-    update_calendars()
     app.run(host="127.0.0.1", debug=True)
--- server/project/wsgi.py
+++ server/project/wsgi.py
@@ -1,4 +1,4 @@
 from server import app
+from server import update_calendars
 
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+# update_calendars() # This will start the update when the Server is started. Disabled while Untis is buggy, because server will not start when this crashes!
