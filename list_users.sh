2c0a933683c6b84209caf41e551542ce9cb3180d
Kurzhalsgiraffe <max-hantschke@web.de>
2024-09-30 01:07:35 +0200

    Update list_users.sh

--- list_users.sh
+++ list_users.sh
@@ -3,10 +3,13 @@
 # Define your SQLite database file
 DB_FILE="instance/database.db"
 
-# Define your SQL query
+# Define your SQL queries
 SQL_QUERY="SELECT id, username, register_date, last_login, last_calendar_pull FROM user;"
+COUNT_QUERY="SELECT COUNT(*) FROM user;"
 
-# Execute the query using sqlite3 command with desired output formatting options
+# Execute the queries using sqlite3 command with desired output formatting options
+
+# Execute the main query and print the results
 sqlite3 "$DB_FILE" <<EOF
 .mode column
 .headers on
@@ -14,3 +17,6 @@ sqlite3 "$DB_FILE" <<EOF
 .nullvalue NULL
 $SQL_QUERY
 EOF
+
+# Count the number of users
+echo "Number of users: $(sqlite3 "$DB_FILE" "$COUNT_QUERY")"
