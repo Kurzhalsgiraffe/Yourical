64bd3a29f53c9b91b6cbee793060289f2dff81c6
Kurzhalsgiraffe <max-hantschke@web.de>
2024-10-02 21:07:51 +0200

    Update list_users.sh

--- list_users.sh
+++ list_users.sh
@@ -4,7 +4,15 @@
 DB_FILE="instance/database.db"
 
 # Define your SQL queries
-SQL_QUERY="SELECT id, username, register_date, last_login, last_calendar_pull FROM user;"
+SQL_QUERY="
+SELECT id,
+       username,
+       register_date,
+       last_login,
+       last_calendar_pull,
+       ROUND(julianday('now') - julianday(last_calendar_pull), 2) AS days_since_last_pull
+FROM user;"
+
 COUNT_QUERY="SELECT COUNT(*) FROM user;"
 
 # Execute the queries using sqlite3 command with desired output formatting options
